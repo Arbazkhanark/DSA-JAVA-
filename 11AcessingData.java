0e92c2dad076a2d8e795efcbfb2c5da2f8f4c6c7
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-09-24 04:56:46 +0530

    Accessing things in JAVA

--- 11AcessingData.java
+++ 11AcessingData.java
@@ -1,12 +1,5 @@
-// Constructor: It helps to create Object,  It have the Same Name as Class.
 
-
-// Default Constructor: Java Automatically create the Empty Parameter Constructor to creating an OBJECT.
-
-// This may also run when Constructor Overloading.......
-
-
-class Teacher{
+class Teacher1{
     //PROPERTIES
     String name;
     int age;
@@ -26,12 +19,16 @@ class Teacher{
 
 
     // --------------------   Constructor    --------------------------
-    public Teacher(){
+    public Teacher1(){
         System.out.println("I am Constructor, call");
 
+        count++;
     // This is Constructor But This is hide all the time but it is there to help to create the Object
     }
-    public Teacher(String newName){
+    public Teacher1(String newName ,int newAge){
+        name=newName;
+        age=newAge;
+        
         System.out.println("I am Second Constructor which have some Parameter");
 
         //This has the Same Name as before but This is also a Constructor 
@@ -44,26 +41,21 @@ class Teacher{
 }
 
 
-class Constructor{
+class Access{
     public static void main(String[] args) {
 
-        Teacher t1=new Teacher();
-        Teacher t2=new Teacher("hey");
+        // Teacher t1=new Teacher();
+        Teacher1 t2=new Teacher1("hey",23);
         //              ^^>This is CONSTRUCTOR, Same name as Object's Class. 
 
-        t1.name="KamalPreet";
-        t1.age=32;
-        t1.sub="Operating System";
+/*
+        t2.name="KamalPreet";
+        t2.age=32;
+        t2.sub="Operating System";
+ */
 
-        t2.name="Dinesh";
-        t2.age=40;
-        t2.sub="N/A";
 
         System.out.println(t2.name+" Age "+t2.age);
-        System.out.println(t1.name+" Age "+t1.age);
-
-        t2.work();
-        t1.work("Daily");
 
 
         System.out.println("Count: "+Teacher.count); // STATIC  helps to access the variable Directly.
