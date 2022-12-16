993b0037939077e10be03e6e5131984231abebba
Ark <arbaazkhanark23@gmail.com>
2022-12-16 22:13:15 +0530

    Sorting

--- SortingAlgo/Bubble_Sort.java
+++ SortingAlgo/Bubble_Sort.java
@@ -3,30 +3,31 @@ package SortingAlgo;
 
 //This code can Sort the elements but it is not a Bubble-Sort!!
 
-// class Sort{
-//     public static void main(String[] args) {
-//         int arr[]={5,10,9,9,2,6,3,1,3};
+/*
+class Sort{
+    public static void main(String[] args) {
+        int arr[]={5,10,9,9,2,6,3,1,3};
 
-//         for(int i=0;i<arr.length;i++){
-//             for(int j=i+1;j<arr.length;j++){
-//                 if(arr[i]>arr[j]){
-//                     int temp=0;
-//                     temp=arr[i];
-//                     arr[i]=arr[j];
-//                     arr[j]=temp;
-//                 }
-//             }
-//             System.out.println(arr[i]);
-//     }
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp=0;
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+            System.out.println(arr[i]);
+    }
 
-// }
-// }
-
-
-
+}
+}
+ */
 
 
-//Real Bubble-Sort
+
+
+//Real Bubble-Sort  (repeatedly swapping the adjacent elements if they are in the wrong order.)
 class BubbleSortExample {  
 
         public static void printArr(int a[]){
