2f88492446900dc40c0a4e66d67e047bbee5fe35
Ark <arbaazkhanark23@gmail.com>
2023-01-11 03:16:09 +0530

    Search in an Infinite Array using binarySearch....

--- NewDataStructure/BinarySearch/BinarySearch/search_in_Rotated_Array.java
+++ NewDataStructure/BinarySearch/BinarySearch/search_in_Rotated_Array.java
@@ -4,19 +4,23 @@ public class search_in_Rotated_Array {
     public static int search(int arr[], int key){
         int start=0;
         int end=arr.length;
+        
+        // if(start>end)return -1;
+        
         while(start<=end){
-            int mid=(start+end)/2;
+            int mid=start+(end-start)/2;
 
+        //Mid    
             if(arr[mid]==key){
                 return mid;
             }
 
     // Left Side
             if(arr[mid]>arr[start]){
-                if(arr[start]<=key && key<arr[mid]){
+                if(arr[start]<=key && arr[mid]>key){
                     end=mid-1;
                 }else{
-                    start=mid+1;
+                    start=mid;
                 }
             
             
@@ -34,7 +38,8 @@ public class search_in_Rotated_Array {
         return -1;
 }
     public static void main(String args[]){
-        int arr[]={20,30,40,50,60,5,10};
+        // int arr[]={20,30,40,50,60,5,10};
+        int arr[]={1,3,4,5,6,7,8,9,10};
         int key=10;                          // last element is not working properly
         int res=search(arr,key);
         System.out.println(res);
