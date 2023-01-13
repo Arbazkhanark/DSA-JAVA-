555edd4049f6971f9607d60aeb8a9d146c67daff
Ark <arbaazkhanark23@gmail.com>
2023-01-14 02:49:11 +0530

    We Link the Nodes && But Here is no head RightNow :(   .

--- NewDataStructure/OverviewDSAbyApnaCollege/CollectionFrameWork/LinkList1.java
+++ NewDataStructure/OverviewDSAbyApnaCollege/CollectionFrameWork/LinkList1.java
@@ -5,12 +5,18 @@ class LL{
         Node n1=new Node(10);
         Node n2=new Node(20);
         Node n3=new Node(30);
+
+        
+        n1.next=n2;
+        n2.next=n3;
+        n3.next=null;
     }
 }
 
 class Node{
     int Data;
     Node next;
+
 
     public Node(int data){
         this.Data=data;
