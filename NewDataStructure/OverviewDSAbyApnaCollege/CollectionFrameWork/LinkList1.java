b820c4b7ce2b98e0fd7b8929d5d2d748cbfc8f16
Ark <arbaazkhanark23@gmail.com>
2023-01-14 02:40:27 +0530

    Made a LinkList class && Node class

---
+++ NewDataStructure/OverviewDSAbyApnaCollege/CollectionFrameWork/LinkList1.java
@@ -0,0 +1,12 @@
+package NewDataStructure.OverviewDSAbyApnaCollege.CollectionFrameWork;
+class LL{
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Node{
+    int Data;
+    Node next;
+}
