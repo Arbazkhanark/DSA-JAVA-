edede28c528abfb7f161fff8970f13ff2239125f
Ark <arbaazkhanark23@gmail.com>
2023-01-31 01:56:56 +0530

    Hashing -HashMap

---
+++ NewDataStructure/Collection/Hashing__HashMap/Quadruple(2||4Sum).java
@@ -0,0 +1,13 @@
+package NewDataStructure.Collection.Hashing__HashMap;
+
+class Quadruple{
+
+    public static int FourSum(int arr[]){
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3,5,0,6};
+        System.out.println(FourSum(arr));
+    }
+}
