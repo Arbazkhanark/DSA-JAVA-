73bd991ead22c8578ad5c9c122e5aa2501a06055
Ark <arbaazkhanark23@gmail.com>
2023-02-02 00:31:00 +0530

    Reverse in Group K

--- NewDataStructure/OverviewDSAbyApnaCollege/CollectionFrameWork/ReverseNodesRecurssion.java
+++ NewDataStructure/OverviewDSAbyApnaCollege/CollectionFrameWork/ReverseNodesRecurssion.java
@@ -1,56 +1,56 @@
-package NewDataStructure.OverviewDSAbyApnaCollege.CollectionFrameWork;
+// package NewDataStructure.OverviewDSAbyApnaCollege.CollectionFrameWork;
 
-public class ReverseNodesRecurssion {
+// public class ReverseNodesRecurssion {
     
-    public static Node reverseNodeRecurssion(Node head){
+//     public static Node reverseNodeRecurssion(Node head){
 
-        if(head==null || head.next==null)return head;
-        Node temp=head.next;
-        Node newNode=reverseNodeRecurssion(head.next);
-        temp.next=head;
-        head.next=null;
-        return newNode;
-    }
+//         if(head==null || head.next==null)return head;
+//         Node temp=head.next;
+//         Node newNode=reverseNodeRecurssion(head.next);
+//         temp.next=head;
+//         head.next=null;
+//         return newNode;
+//     }
 
-    public static void printLL(Node head){
-        Node curr=head;
-        while(curr!=null){
-            System.out.println(curr.data);
-            curr=curr.next;
-        }
-    }
+//     public static void printLL(Node head){
+//         Node curr=head;
+//         while(curr!=null){
+//             System.out.println(curr.data);
+//             curr=curr.next;
+//         }
+//     }
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
-        Node n1=new Node(10);
-        Node n2=new Node(20);
-        Node n3=new Node(60);
-        Node n4=new Node(30);
-        Node n5=new Node(40);
-        Node n6=new Node(50);
+//         Node n1=new Node(10);
+//         Node n2=new Node(20);
+//         Node n3=new Node(60);
+//         Node n4=new Node(30);
+//         Node n5=new Node(40);
+//         Node n6=new Node(50);
     
-        Node head=n1;
-        n1.next=n2;
-        n2.next=n3;
-        n3.next=n4;
-        n4.next=n5;
-        n5.next=n6;
+//         Node head=n1;
+//         n1.next=n2;
+//         n2.next=n3;
+//         n3.next=n4;
+//         n4.next=n5;
+//         n5.next=n6;
 
-        printLL(head);
-        printLL(reverseNodeRecurssion(head));
+//         printLL(head);
+//         printLL(reverseNodeRecurssion(head));
 
-    }
-
-
-
-}
+//     }
 
 
-class Node{
-    int data;
-    Node next;
 
-    public Node(int data){
-        this.data=data;
-    }
-}
+// }
+
+
+// class Node{
+//     int data;
+//     Node next;
+
+//     public Node(int data){
+//         this.data=data;
+//     }
+// }
