f87704d577681844938c619fecc2b6170b25aa2a
Ark <arbaazkhanark23@gmail.com>
2023-02-11 22:28:33 +0530

    max histo

--- NewDataStructure/Collection/Hashing__HashMap/Stack/StackProblems/MaxAreaHistogram.java
+++ NewDataStructure/Collection/Hashing__HashMap/Stack/StackProblems/MaxAreaHistogram.java
@@ -36,17 +36,35 @@ class AreaOfMaximumHistogram{
         // int k=-1;
         int k=0;
         for(int i=0;i<arr.length;i++){
-           int prev=stack.peek();
             if(stack.isEmpty()){
                 stack.push(-1);
                 ps[k++]=-1;
-            }else if(arr[i]>arr[prev]){
-                s
+            }else{
+                int prev=i-1;
+                int curr=i;
+                if(prev<curr){
+                    stack.push(prev);
+                    ps[k++]=prev;
+                    prev--;
+                }else{
+                    stack.pop();
+                    ps[k++]=-1;
+                }
             }
+        }
+
+
+
+
+        for (int x : ps) {
+            System.out.println(x);
         }
     }
 
     public static void main(String[] args) {
-        int arr[]={5,3,5,7,1};
+        int arr[]={5,3,5,7,6,4,1};
+        previous_Smallest(arr);
+        // int temp=5
+        // System.out.println(2);
     }
 }
