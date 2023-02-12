8c22fbde42e26fa8376f2503570977dc7f26b568
Ark <arbaazkhanark23@gmail.com>
2023-02-12 23:33:10 +0530

    Implimentation of Queue using LL

---
+++ NewDataStructure/Collection/Hashing__HashMap/Queue/ImplimentationOfQueueUsingLL.java
@@ -0,0 +1,44 @@
+package NewDataStructure.Collection.Hashing__HashMap.Queue;
+
+public class ImplimentationOfQueueUsingLL {
+    Node front;
+    Node rear;
+
+    void enqueue(int data){
+        Node newNode =new Node(data);
+        if(!isEmpty()){
+            rear.next=newNode;
+            rear=newNode;
+            rear.next=null;
+        }else{
+            front=rear=newNode;
+        }
+    }  
+
+    int dequeue()throws Exception {
+        if(isEmpty()) throw new Exception("Queue is Clean!!!");
+        int deleted=front.data;
+        front=front.next;
+        return deleted;
+    }
+    
+    Boolean isEmpty(){
+        if(front==null)return true;
+        return false;
+    }
+    
+    int getFront() throws Exception{
+        if(isEmpty()) throw new Exception("Queue is Empty!!!!!");
+        return front.data;
+    }
+
+}
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data=data;
+    }
+}
---
+++ NewDataStructure/Collection/Hashing__HashMap/Queue/UsingQueueImplimentationLL.java
@@ -0,0 +1,17 @@
+package NewDataStructure.Collection.Hashing__HashMap.Queue;
+
+public class UsingQueueImplimentationLL {
+
+    public static void main(String[] args) throws Exception {
+        ImplimentationOfQueueUsingLL q=new ImplimentationOfQueueUsingLL();
+    
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+
+        System.out.println(q.dequeue());
+        System.out.println(q.getFront());
+        System.out.println(q.isEmpty());
+    }
+
+}
